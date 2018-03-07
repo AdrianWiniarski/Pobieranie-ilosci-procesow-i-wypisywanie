45ca8d8fd2ec0a93c6317b7599142c2dfdc04c38
Adrian Winiarski <winiak1996@gmail.com>
2018-03-07 10:45:45 +0100

    Update main1.c

--- main1.c
+++ main1.c
@@ -8,22 +8,25 @@
 
 void main(int argc, char* argv[]){ 
   int pid,status,i,j;
-for(i=0;i<argc-1 ;i++)
-{ 
-  if((pid = fork()) == 0) { /* Proces potomny pot ---*/ 
-     printf("potomny = %d \n",getpid()); 
+  for(i=0;i<argc-1 ;i++){ 
+    if((pid = fork()) == 0) { /* Proces potomny pot ---*/ 
+        printf("potomny = %d \n",getpid()); 
 	for(j=i;j < atoi(argv[i+1]);++j){
-		printf("potomny = %d ,krok %d \n",getpid(), j);
-}
-     exit(i);
- 			  }   
- /* Proces macierzysty */ 
-}
-	printf("Macierzysty = %d \n",getpid()); 
-for(i=0;i<argc-1 ;i++){
+	   printf("potomny = %d ,krok %d \n",i+1, j);
+	    sleep(1);        
+ 	}
+        exit(i);
+    }   
+
+  }
+  /* Proces macierzysty */ 
+  for(j=0;j<atoi(argv[1]);++j){
+  printf("Macierzysty = %d \n",getpid()); 
+  }  
+  for(i=0;i<argc-1 ;i++){
 	pid = wait(&status);
 	printf("Proces %d zakończony status: %d\n",pid,status/256); 
-			  }
-      
+  }
+ 
  
 } 
